1d17300bd2391602f999f627ca7285504fa95840
Alex Karpinski <me@alexkarpinski.com>
2019-07-21 23:14:50 +0000

    pretty buttons that attach to stuff

--- static/main.js
+++ static/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-
+    $("a.question-response").click(function(event){
+        question_response_from_user = $(this).attr('href').substr(1);
+        var request_url = `/respond/${question.q_id}/${question_response_from_user}`
+        fetch(request_url)
+        .then(function(response) {
+            return response.text().then(function(text) {
+                $("#user-response-badge").text(text);
+            });
+        });
+    })
 });
